2869b2ba52706b3cebda64c04818a533babe6a61
Oleg Kudimov <ovkudimov@gmail.com>
2021-08-19 15:05:19 +0300

    Initial Commit

---
+++ TravelApp/TravelAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelAppApp.swift
+//  TravelApp
+//
+//  Created by Oleg Kudimov on 8/19/21.
+//
+
+import SwiftUI
+
+@main
+struct TravelAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
